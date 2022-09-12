398ca5b32442ef15d4d7b83c1c6d361bbcf6332c
Esther Muiga <esthermuiga3@gmail.com>
2022-09-12 04:32:29 -0700

    Adding a main file

---
+++ 0x05-python-exceptions/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+def safe_print_integer(value):
+def safe_print_list_integers(my_list=[], x=0):
+def safe_print_division(a, b):
+def list_division(my_list_1, my_list_2, list_length):
+def raise_exception():
+def raise_exception_msg(message=""):
+def safe_print_integer_err(value):
+def safe_function(fct, *args):
+void print_python_list(PyObject * p);
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
