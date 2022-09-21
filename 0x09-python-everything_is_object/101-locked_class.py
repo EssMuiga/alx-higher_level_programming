78ed1274487c8d1ca14bdc8a59dc33f1aa7468fd
Esther Muiga <esthermuiga3@gmail.com>
2022-09-20 22:58:04 -0700

    Locked class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-"""locked class"""
-
-class LockedClass():
-    """create instance and attribute 'first name'"""
-    __slots__ = ['first name']
+class LockedClass:
+    """Locked class that lets the user create instance attribute 'first_name'"""
+    __slots__ = ['first_name']
