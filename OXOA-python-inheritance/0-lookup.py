23e28ee7c11053b1f14f3306fb9f96319c394d70
Esther Muiga <esthermuiga3@gmail.com>
2022-09-27 02:01:47 -0700

    First commit

---
+++ OXOA-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+The file contains the look up function
+"""
+
+
+def lookup(obj):
+    """returns a list of available attributes and methods of an object"""
+    return dir(obj)
