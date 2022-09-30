c39e7e3a8e384b3bbf675d9f891a8808e8a0f6cc
Esther Muiga <esthermuiga3@gmail.com>
2022-09-29 22:54:19 -0700

    String to JSON

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -7,6 +7,6 @@ It has a to_json_string function
 import json
 
 
-def to_json_string)my_obj):
+def to_json_string(my_obj):
     """returns the JSON represantation of an object (string)"""
     return json.dumps(my_obj)
