b161f7e457f9deb242ededda3d3e4cd3c594814d
Esther Muiga <esthermuiga3@gmail.com>
2022-11-03 04:40:38 -0700

    Updating the tenth file

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,5 +1,5 @@
 -- lists all records of second_table
 -- records ordered by descending score
-SELECT 'score', 'name'
-FROM 'second_table'
-ORDER BY 'score' DESC;
+SELECT `score`, `name`
+FROM `second_table`
+ORDER BY `score` DESC;
